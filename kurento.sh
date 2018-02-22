a867d11a09cd8f21ab7ce3701c23e17b363fe0fc
bean <jojo13572001@yahoo.com.tw>
2018-02-22 16:22:06 +0800

    change kurento to restart

--- kurento.sh
+++ kurento.sh
@@ -1,6 +1,8 @@
 #/bin/bash
-sudo service kurento-media-server-6.0 stop
-sleep 2
-sudo service kurento-media-server-6.0 start
+#sudo service kurento-media-server-6.0 stop
+#sleep 2
+#sudo service kurento-media-server-6.0 start
+sudo service kurento-media-server-6.0 restart
+
 #http-server -p 8443 -S -C keys/defaultCertificate.pem -K keys/defaultCertificate.pem
 http-server -p 8443
